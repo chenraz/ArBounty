8dfec9438e32fdfc22691e4e9de773ce6b9b349e
Chen Raz <chen_raz@hotmail.com>
2022-02-16 15:28:19 +0200

    Swap BrowserRoter with HashRouter
    Closes: #1

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as BrowserRouter } from "react-router-dom";
 
 import './index.css';
 import App from './App';
